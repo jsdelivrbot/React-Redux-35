c386067625f371b9d4f12bc56d11e1c77f1bba80
Yurii Zadorozhnyi <yurij_zadoroznij@ukr.net>
2017-09-27 22:34:45 +0300

    upload files to repository

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'JavaScript', pages: 321},
+    {title: 'Python', pages: 212},
+    {title: 'Objective-C', pages: 222},
+    {title: 'Java', pages: 152},
+  ];
+}
